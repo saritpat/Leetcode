8189a6ab9fb95f57cd6b3d28d0559c9b6e88ea86
saritpat <69256964+saritpat@users.noreply.github.com>
2024-09-28 16:54:38 +0700

    ➕ Add new file

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        st = []
+        
+        for i in s:
+            if i == '[' or i == '{' or i == '(':
+                st.append(i)
+
+            elif len(st) != 0:
+                if i == ']' and st[-1] == '[':
+                    st.pop()
+                elif i == '}' and st[-1] == '{':
+                    st.pop()
+                elif i == ')' and st[-1] == '(':
+                    st.pop()
+                else:
+                    return False
+
+            else:
+                return False
+        
+        if len(st) == 0:
+            return True
+        else:
+            return False
+    
+# (()[]) (]) ([)]
+S = Solution()
+x = "(()[])"
+print(S.isValid(x))
+
+
+# -----------------------------------------------------
+# Best solution
+
+# class Solution(object):
+#     def isValid(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+
+# # (()[]) (]) ([)]
+# S = Solution()
+# s = "(()[])"
+# print(S.isValid(s))
