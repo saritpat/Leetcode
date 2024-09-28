1f1900a02e06f0bd7f5ebe99088d329378d1c535
saritpat <69256964+saritpat@users.noreply.github.com>
2024-09-28 16:32:10 +0700

    🆕 Add new file

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        result = strs[0]
+        length = len(result)
+
+        for i in range(1, len(strs)):
+            while strs[i].find(result) != 0:
+                result = result[:length - 1]
+                length -= 1
+
+            if not result:
+                return ""
+
+        return result
+
+
+        
+S = Solution()
+s = ["flower","flow","flight"]
+print(S.longestCommonPrefix(s))
+
+
+# -----------------------------------------------------
+# Best solution
+
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: str
+#         """
+
+# S = Solution()
+# s = ["flower","flow","flight"]
+# print(S.longestCommonPrefix(s))
