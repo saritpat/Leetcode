4a93d6b830e259aab86a404e57bbd36a4ba29f43
saritpat <69256964+saritpat@users.noreply.github.com>
2024-09-28 18:24:52 +0700

    ✨ NEW FILE

---
+++ 27. Remove Element.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        length = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                nums[i] = '_'
+                length += 1
+        
+        for i in range(length):
+            nums.remove('_')
+            
+        k = len(nums)
+        return k
+
+s = Solution()
+nums = [3,2,2,3]
+v = 3
+print(s.removeElement(nums, v))
+
+
+# -----------------------------------------------------
+# Best solution
+
+# class Solution(object):
+#     def removeElement(self, nums, val):
+#         """
+#         :type nums: List[int]
+#         :type val: int
+#         :rtype: int
+#         """
+#         length = 0
+#         for i in range(len(nums)):
+#             if nums[i] == val:
+#                 nums[i] = '_'
+#                 x += 1
+        
+#         for i in range(length):
+#             nums.remove('_')
+            
+#         k = len(nums)
+#         return k
+
+# s = Solution()
+# nums = [3,2,2,3]
+# v = 3
+# print(s.removeElement(nums, v))
